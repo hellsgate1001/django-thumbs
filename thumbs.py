4183c6ab23724d7012d154109b59bec35ccd1ac8
antonio.mele <antonio.mele@476cbdc6-d08b-11dd-aa7b-9fdd872f0417>
2008-12-23 12:40:59 +0000

    Added nice comments

--- thumbs.py
+++ thumbs.py
@@ -1,10 +1,8 @@
 # -*- encoding: utf-8 -*-
 """
-django-thumbnails by Antonio Melé
+django-thumbs by Antonio Melé
 http://django.es
 """
-
-
 from django.db.models import ImageField
 from django.db.models.fields.files import ImageFieldFile
 from PIL import Image
@@ -12,6 +10,19 @@ from django.core.files.base import ContentFile
 import cStringIO
 
 def generate_thumb(img, thumb_size, format):
+    """
+    Generates a thumbnail image and returns a ContentFile object with the thumbnail
+    
+    Parameters:
+    ===========
+    img         File object
+    
+    thumb_size  desired thumbnail size, ie: (200,120)
+    
+    format      format of the original image ('jpeg','gif','png',...)
+                (this format will be used for the generated thumbnail, too)
+    """
+    
     img.seek(0) # see http://code.djangoproject.com/ticket/8222 for details
     image = Image.open(img)
     
@@ -51,10 +62,7 @@ def generate_thumb(img, thumb_size, format):
 
 class ImageWithThumbsFieldFile(ImageFieldFile):
     """
-    usage:
-    model.field_name.url
-    model.field_name.url_125x125
-    model.field_name.url_600x400
+    See ImageWithThumbsField for usage example
     """
     def __init__(self, *args, **kwargs):
         super(ImageWithThumbsFieldFile, self).__init__(*args, **kwargs)
@@ -82,9 +90,8 @@ class ImageWithThumbsFieldFile(ImageFieldFile):
                 split = self._name.rsplit('.',1)
                 thumb_name = '%s.%sx%s.%s' % (split[0],w,h,split[1])
                 
-                # here goes your thumbnailing function
+                # you can use another thumbnailing function if you like
                 thumb_content = generate_thumb(content, size, split[1])
-                
                 
                 thumb_name_ = self.storage.save(thumb_name, thumb_content)        
                 
@@ -107,7 +114,46 @@ class ImageWithThumbsFieldFile(ImageFieldFile):
 class ImageWithThumbsField(ImageField):
     attr_class = ImageWithThumbsFieldFile
     """
-    sizes=((125,125),(600,400), ...)
+    Usage example:
+    ==============
+    photo = ImageWithThumbsField(upload_to='images', sizes=((125,125),(300,200),)
+    
+    To retrieve image URL, exactly the same way as with ImageField:
+        my_object.photo.url
+    To retrieve thumbnails URL's just add the size to it:
+        my_object.photo.url_125x125
+        my_object.photo.url_300x200
+    
+    Note: The 'sizes' attribute is not required. If you don't provide it, 
+    ImageWithThumbsField will act as a normal ImageField
+        
+    How it works:
+    =============
+    For each size in the 'sizes' atribute of the field it generates a 
+    thumbnail with that size and stores it following this format:
+    
+    available_filename.[width]x[height].extension
+
+    Where 'available_filename' is the available filename returned by the storage
+    backend for saving the original file.
+    
+    Following the usage example above: For storing a file called "photo.jpg" it saves:
+    photo.jpg          (original file)
+    photo.125x125.jpg  (first thumbnail)
+    photo.300x200.jpg  (second thumbnail)
+    
+    With the default storage backend if photo.jpg already exists it will use these filenames:
+    photo_.jpg
+    photo_.125x125.jpg
+    photo_.300x200.jpg
+    
+    Note: django-thumbs assumes that if filename "any_filename.jpg" is available 
+    filenames with this format "any_filename.[widht]x[height].jpg" will be available, too.
+    
+    To do:
+    ======
+    Add method to regenerate thubmnails
+    
     """
     def __init__(self, verbose_name=None, name=None, width_field=None, height_field=None, sizes=None, **kwargs):
         self.verbose_name=verbose_name
